7085e9b5fc882bc3a4a9f7f3743ad96ddd312fb9
surajkrayons-dev <suraj.krayons@gmail.com>
2026-02-27 10:49:25 +0530

    trigger deploy

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/",   // ✅ PRODUCTION KE LIYE MUST
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
